24fbe912ab0edc51d6321c07b7386d27726a38ef
Raul Andrial <rauljandrial@gmail.com>
2022-11-11 18:36:30 +0000

    Added random dog images

--- src/js/async-functions.js
+++ src/js/async-functions.js
@@ -21,3 +21,47 @@ export async function fetchContacts(agenda_slug) {
         throw new Error(`Error in fecthing contacts for agenda ${agenda_slug}`);
     }
 }
+
+export async function deleteContact(contact_id) {
+    
+    try {
+        return await fetch(`${contactEndpoint}${contact_id}`, {
+            method: 'DELETE',
+            headers: {'Content-Type': 'application/json'}
+        });
+    } catch (err) {
+        throw new Error(`Error in deleting contact ${contact_id}`);
+    }
+}
+
+export async function createContact(contact) {
+
+    try {
+        return await fetch(contactEndpoint, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(contact)
+        });
+    } catch (err) {
+        throw new Error(`Error in creating contact: ${contact}`);
+    }
+}
+
+export async function updateContact(contact) {
+
+    try {
+        return await fetch(`${contactEndpoint}${contact.id}`, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                full_name: contact.full_name,
+                email: contact.email,
+                agenda_slug: contact.agenda_slug,
+                address: contact.address,
+                phone: contact.phone
+            })
+        });
+    } catch (err) {
+        throw new Error(`Error in updating contact ${contact.id}`);
+    }
+}
--- src/js/component/contact-img.jsx
+++ src/js/component/contact-img.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import contactImg from '../../img/me.jpg'
+import React, { useEffect, useState} from 'react';
 
 const ContactImg = () => {
 
     const imgMaxDim = '225px';
+    const [dogImg, setDogImg] = useState('');
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(res => res.json())
+            .then(obj => setDogImg(obj.message));
+    }, []);
 
     return (
         <img
-            src={contactImg}
+            src={dogImg}
             alt='Contact Imgage'
             className='img-fluid rounded-circle'
             style={{maxHeight: imgMaxDim, maxWidth: imgMaxDim}}
